d47cb64ac0519073073f7f4053e6d4fb6bd6f729
Yann Hoffmann <y.hoffmann@talkwalker.com>
2023-01-13 12:14:19 +0100

    Add tunnels to auth_vpn and rename to connect_work

--- scripts/connect_work.sh
+++ scripts/connect_work.sh
@@ -1,3 +1,4 @@
+# auth_vpn.sh
 VPN_USER="y.hoffmann"
 TW_PASSWORD="$(pass show talkwalker)"
 
@@ -13,3 +14,12 @@ sudo openvpn \
 	--auth-user-pass "$CREDS"
 
 rm "$CREDS"
+
+
+## set up tunnels
+# git tunnels
+ssh -L 3333:git.talkwalker.net:22 work
+# VNC
+ssh -L 9999:localhost:5901 work
+# audience grpc testing
+ssh -L 4044:wc2n8:4044 work
